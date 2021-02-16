6aa57a858d0aef3747cde7f4c107c8bc1048a136
장은경 <ekjang@datastreams.co.kr>
2021-02-16 10:43:57 +0900

    aws url 추가

--- front/src/lotto/define/UrlDefine.js
+++ front/src/lotto/define/UrlDefine.js
@@ -1,5 +1,6 @@
 // const server_url = "http://localhost:3001"
-const server_url = "http://192.168.2.88:9999/jr" //spring
+// const server_url = "http://192.168.2.88:9999/jr" //desktop
+const server_url = "http://3.140.125.81:9999/jr" //aws
 // const server_url = "https://ekjang.github.io/myapp-jerrytto"
 
 export default server_url;
